550352afb8fa948ca4777f0443d43c053ed60aa6
junios <junios@junios.net>
2020-07-01 14:20:03 +0900

    yaml를 이용한 다국어 지원 Exception을 다국어 출력 lang 파라미터를 다국어 지원 언어 선택 변수로 등록

--- src/main/java/net/junios/api/controller/v1/UserController.java
+++ src/main/java/net/junios/api/controller/v1/UserController.java
@@ -11,6 +11,11 @@ import net.junios.api.model.response.ListResult;
 import net.junios.api.model.response.SingleResult;
 import net.junios.api.repository.UserJpaRepo;
 import net.junios.api.service.ResponseService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,12 +36,12 @@ public class UserController {
         return responseService.getListResult(userJpaRepo.findAll());
     }
 
-    @ApiOperation(value = "회원 단건 조회", notes = "userId로 회원을 조회한다")
-    @GetMapping(value = "/user/{msrl}")
-    public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl) {
-        // 결과데이터가 단일건인경우 getBasicResult를 이용해서 결과를 출력한다.
-        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
-    }
+//    @ApiOperation(value = "회원 단건 조회", notes = "userId로 회원을 조회한다")
+//    @GetMapping(value = "/user/{msrl}")
+//    public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl) {
+//        // 결과데이터가 단일건인경우 getBasicResult를 이용해서 결과를 출력한다.
+//        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
+//    }
 
 //    @ApiOperation(value = "회원 단건 조회", notes = "userId로 회원을 조회한다")
 //    @GetMapping(value = "/user/{userId}")
@@ -45,6 +50,13 @@ public class UserController {
 ////        return responseService.getSingleResult(userJpaRepo.findById(userId).orElseThrow(Exception::new));
 //        return responseService.getSingleResult(userJpaRepo.findById(userId).orElseThrow(CUserNotFoundException::new));
 //    }
+
+    @ApiOperation(value = "회원 단건 조회", notes = "userId로 회원을 조회한다")
+    @GetMapping(value = "/user/{msrl}")
+    public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl, @ApiParam(value = "언어", defaultValue = "ko") @RequestParam String lang) {
+        // 결과데이터가 단일건인경우 getBasicResult를 이용해서 결과를 출력한다.
+        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
+    }
 
     @ApiOperation(value = "회원 입력", notes = "회원을 입력한다")
     @PostMapping(value = "/user")
@@ -79,4 +91,31 @@ public class UserController {
         // 성공 결과 정보만 필요한경우 getSuccessResult()를 이용하여 결과를 출력한다.
         return responseService.getSuccessResult();
     }
+
+
+    @Autowired
+    private MessageSource messageSource;
+
+    private static final Logger logger = LogManager.getLogger(UserController.class);
+
+
+    @ApiOperation(value="Test", notes = "이것은 테스트 입니다.")
+    @GetMapping(value = "/test")
+    @ResponseBody
+    public String test(@RequestParam String lang) {
+        //logger.debug((getMessage("test.code") + getMessage("userNotFound.msg")));
+        //logger.debug("message {}", messageSource == null);
+        System.out.println(getMessage("test.code")+getMessage("test.msg"));
+        return "test";//(getMessage("test.code") + getMessage("userNotFound.msg"));
+    }
+
+    // code정보에 해당하는 메시지를 조회합니다.
+    private String getMessage(String code) {
+        return getMessage(code, null);
+    }
+    // code정보, 추가 argument로 현재 locale에 맞는 메시지를 조회합니다.
+    private String getMessage(String code, Object[] args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+
 }
